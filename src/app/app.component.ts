532329aa7641664ed80393011ea3ae1ebf32f99e
piotroszko <piotroszko2@gmail.com>
2021-05-01 16:11:45 +0200

    Students table moved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,6 @@
-import { Component, VERSION, OnInit } from "@angular/core";
-import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
-import { Post } from "./post";
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import "rxjs/add/operator/map";
-import {
-  Subject,
-  asapScheduler,
-  pipe,
-  of,
-  from,
-  interval,
-  merge,
-  fromEvent,
-  Observable
-} from "rxjs";
 
 import "rxjs/add/operator/catch";
 import "rxjs/add/operator/retry";
@@ -23,35 +11,23 @@ import "rxjs/add/observable/of";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
-  readonly ROOT_URL = "https://uwm-gr1.azurewebsites.net/api/Student";
-
-  posts: Observable<any>;
-  newPost: Observable<any>;
-
+export class AppComponent {
+  ROOT_URL = "https://uwm-gr1.azurewebsites.net/api/Student";
   constructor(private http: HttpClient) {}
+  // createPost() {
+  //   const data: Post = {
+  //     id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  //     surname: "Test1",
+  //     name: "Piotr",
+  //     index: "12121212"
+  //   };
 
-  getPosts() {
-    this.posts = this.http.get(this.ROOT_URL);
-  }
-  ngOnInit() {
-    this.getPosts();
-  }
-
-  createPost() {
-    const data: Post = {
-      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      surname: "Test1",
-      name: "Piotr",
-      index: "12121212"
-    };
-
-    this.newPost = this.http
-      .post(this.ROOT_URL, data)
-      .retry(3)
-      .catch(err => {
-        console.log(err);
-        return Observable.of(err);
-      });
-  }
+  //   this.newPost = this.http
+  //     .post(this.ROOT_URL, data)
+  //     .retry(3)
+  //     .catch(err => {
+  //       console.log(err);
+  //       return Observable.of(err);
+  //     });
+  // }
 }
